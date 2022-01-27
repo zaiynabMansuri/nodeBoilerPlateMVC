feda75d9bb41f6d5499a56588ba567c510140772
zaiynabMansuri <zaiynab.mansuri@dhiwise.com>
2022-01-27 05:05:00 +0000

    Code Synced by DhiWise

---
+++ middleware/loginUser.js
@@ -0,0 +1,39 @@
+/**
+ * loginUser.js
+ * @description :: middleware that verifies user's JWT token
+ */
+
+const jwt = require('jsonwebtoken');
+const deviceSecret = require('../constants/authConstant').JWT.DEVICE_SECRET;
+const adminSecret = require('../constants/authConstant').JWT.ADMIN_SECRET;
+
+/**
+ * @description : middleware for authenticate user with JWT token
+ * @param {obj} req : request of route.
+ * @param {obj} res : response of route.
+ * @param {callback} next : executes the next middleware succeeding the current middleware.
+ */
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    let url = req.originalUrl;
+    let secret = '';
+    if (url.includes('device')){
+      secret = deviceSecret;
+    }
+    else if (url.includes('admin')){
+      secret = adminSecret;
+    }
+    jwt.verify(token,secret, (err, user) => {
+      if (err) {
+        return res.unAuthorizedRequest();
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.unAuthorizedRequest();
+  }
+};
+module.exports = authenticateJWT;
---
+++ routes/admin/index.js
@@ -0,0 +1,15 @@
+/**
+ * index.js
+ * @description :: index route file of admin platform.
+ */
+
+const express =  require('express');
+const router =  express.Router();
+router.use('/admin/auth',require('./auth'));
+router.use(require('./userRoutes'));
+router.use(require('./roleRoutes'));
+router.use(require('./projectRouteRoutes'));
+router.use(require('./routeRoleRoutes'));
+router.use(require('./userRoleRoutes'));
+
+module.exports = router;
